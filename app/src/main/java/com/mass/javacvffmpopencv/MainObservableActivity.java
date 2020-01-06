55191a804f45fade79e3169c01d516c438bdf3b8
jam_he <jam_he@163.com>
2020-01-06 16:51:36 +0800

    init1

--- app/src/main/java/com/mass/javacvffmpopencv/MainObservableActivity.java
+++ app/src/main/java/com/mass/javacvffmpopencv/MainObservableActivity.java
@@ -2,15 +2,58 @@ package com.mass.javacvffmpopencv;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.util.Log;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+
 
 public class MainObservableActivity extends AppCompatActivity {
+
+    private String TAG = "MainObservableActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_observable);
-        Toast.makeText(this,"fff",Toast.LENGTH_LONG).show();
 
+        Observable observable = Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+                Log.d(TAG, "=========================currentThread name: " + Thread.currentThread().getName());
+                emitter.onNext(1);
+                emitter.onNext(2);
+                emitter.onNext(2);
+                emitter.onComplete();
+            }
+        });
+
+        Observer<Integer> observer=new Observer<Integer>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.d(TAG, "======================onSubscribe");
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                Log.d(TAG, "======================onNext " + integer);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d(TAG, "======================onError");
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "======================onComplete");
+            }
+
+
+        };
+        observable.subscribe(observer);
     }
 }
