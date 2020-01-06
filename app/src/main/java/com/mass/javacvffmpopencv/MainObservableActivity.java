0cbfeb7710ee2f4e05afb903c9f09082f97d0e7d
jam_he <jam_he@163.com>
2020-01-06 17:42:09 +0800

    init1

--- app/src/main/java/com/mass/javacvffmpopencv/MainObservableActivity.java
+++ app/src/main/java/com/mass/javacvffmpopencv/MainObservableActivity.java
@@ -3,48 +3,58 @@ package com.mass.javacvffmpopencv;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Observer;
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 
 public class MainObservableActivity extends AppCompatActivity {
 
-    private String TAG = "MainObservableActivity";
+    private final String TAG = "MainObservableActivity";
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_observable);
+        tv=findViewById(R.id.tvtext);
 
         Observable observable = Observable.create(new ObservableOnSubscribe<Integer>() {
             @Override
             public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
-                Log.d(TAG, "=========================currentThread name: " + Thread.currentThread().getName());
+                Log.d(TAG, "Observable =========================currentThread name: " + Thread.currentThread().getName());
                 emitter.onNext(1);
+                Thread.sleep(10000);
                 emitter.onNext(2);
-                emitter.onNext(2);
+                Thread.sleep(10000);
+                emitter.onNext(3);
+                Thread.sleep(10000);
                 emitter.onComplete();
             }
-        });
+        }).subscribeOn(Schedulers.newThread()).observeOn(AndroidSchedulers.mainThread());
 
         Observer<Integer> observer=new Observer<Integer>() {
             @Override
             public void onSubscribe(Disposable d) {
-                Log.d(TAG, "======================onSubscribe");
+                Log.d(TAG, "observer========================= currentThread name: " + Thread.currentThread().getName());
             }
 
             @Override
             public void onNext(Integer integer) {
+                tv.setText(integer+"");
                 Log.d(TAG, "======================onNext " + integer);
             }
 
             @Override
             public void onError(Throwable e) {
-                Log.d(TAG, "======================onError");
+                Log.d(TAG, "======================onError"+e.getMessage());
             }
 
             @Override
