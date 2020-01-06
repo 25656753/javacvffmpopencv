24ee20ac6a450a25e6ea6c6cb1f58fdf0416f62f
jam_he <jam_he@163.com>
2020-01-06 13:49:39 +0800

    init1

--- app/src/main/java/com/mass/javacvffmpopencv/MainObservableActivity.java
+++ app/src/main/java/com/mass/javacvffmpopencv/MainObservableActivity.java
@@ -2,6 +2,7 @@ package com.mass.javacvffmpopencv;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainObservableActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainObservableActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_observable);
+        Toast.makeText(this,"fff",Toast.LENGTH_LONG).show();
     }
 }
