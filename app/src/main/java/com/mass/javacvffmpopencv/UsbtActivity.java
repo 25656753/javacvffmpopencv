4754293531ebe7d76ca10810f424e05813878e72
Admin <25656753@qq.com>
2020-04-22 11:42:42 +0800

    init1

--- app/src/main/java/com/mass/javacvffmpopencv/UsbtActivity.java
+++ app/src/main/java/com/mass/javacvffmpopencv/UsbtActivity.java
@@ -7,8 +7,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.hardware.usb.UsbAccessory;
+import android.hardware.usb.UsbConstants;
 import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbDeviceConnection;
+import android.hardware.usb.UsbEndpoint;
+import android.hardware.usb.UsbInterface;
 import android.hardware.usb.UsbManager;
+import android.hardware.usb.UsbRequest;
 import android.os.Bundle;
 import android.os.ParcelFileDescriptor;
 import android.os.Parcelable;
@@ -23,8 +28,11 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -32,6 +40,9 @@ public class UsbtActivity extends AppCompatActivity {
     private UsbManager usbManager;
     private ServerSocket mServerSocket;
     private Boolean mCanRun=true;
+    private UsbDeviceConnection mUsbDeviceConnection;
+    private UsbEndpoint mUsbEndpoint_in,mUsbEndpoint_out;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,7 +51,7 @@ public class UsbtActivity extends AppCompatActivity {
           @Override
           public void onClick(View v) {
 
-              try {
+            /*  try {
 
                   mServerSocket = new ServerSocket(9000);
               } catch (IOException e) {
@@ -62,10 +73,10 @@ public class UsbtActivity extends AppCompatActivity {
 
                           }
                       }
-                  }).start();
+                  }).start();*/
 
 
-
+              hostModel(UsbtActivity.this);
           }
       });
     }
@@ -73,117 +84,138 @@ public class UsbtActivity extends AppCompatActivity {
 
     public static final String action_usb_permission = "org.zhuhailong.myviewproject.permission";
 
-
-
-    public void hostAccessory(Context context) {
+    public void hostModel(Context context) {
 
         //获取UsbManager
-        usbManager = (UsbManager) context.getSystemService(Context.USB_SERVICE);
+         usbManager = (UsbManager) context.getSystemService(Context.USB_SERVICE);
 
         //获取usb设备列表
-        UsbAccessory[] accessoryList = usbManager.getAccessoryList();
         HashMap<String, UsbDevice> deviceList = usbManager.getDeviceList();
-        Toast.makeText(context, "accessoryList="+accessoryList+"---->deviceList"+deviceList.size(), Toast.LENGTH_SHORT).show();
-        if (accessoryList == null || accessoryList.length <= 0) {
-            Toast.makeText(context, "未查询到对应设备", Toast.LENGTH_SHORT).show();
-            return;
-        }
 
-        UsbAccessory mUsbAccessory = null;
-        for (UsbAccessory usbAccessory : accessoryList) {
-            if (filterDevice(usbAccessory)) {//判断是否使我们要连接的usb设备
+        Set<Map.Entry<String, UsbDevice>> entries = deviceList.entrySet();
+
+        UsbDevice mUsbDevice = null;
+
+        for (Map.Entry<String, UsbDevice> entry : entries) {
+            UsbDevice value = entry.getValue();
+            String key = entry.getKey();
+
+            if (filterDevice(key, value)) {//判断是否使我们要连接的usb设备
+                mUsbDevice = value;
                 break;
             }
         }
 
-        if (usbManager.hasPermission(mUsbAccessory)) {//检测是否有usb权限
-            requestPermission(mUsbAccessory,usbManager);//请求usb权限
+        if (usbManager.hasPermission(mUsbDevice)) {//检测是否有usb权限
+            requestPermission(mUsbDevice,usbManager);
         } else {
-            connect(mUsbAccessory);//连接usb设备
+            connect(mUsbDevice);
         }
 
     }
 
     /**
      * 请求usb权限
-     *
      * @param usbManager
      * @param parcelable
      */
-    public void requestPermission(Parcelable parcelable, UsbManager usbManager) {
-        UsbAccessory usbAccessory = (UsbAccessory) parcelable;
-        if (!usbManager.hasPermission(usbAccessory)) {
+    public void requestPermission(Parcelable parcelable,UsbManager usbManager) {
+        UsbDevice usbDevice = (UsbDevice) parcelable;
+        if (!usbManager.hasPermission(usbDevice)) {
             IntentFilter intentFilter = new IntentFilter(action_usb_permission);
             registerReceiver(mMyBroadcastReceiver, intentFilter);
             PendingIntent broadcast = PendingIntent.getBroadcast(this, 0, new Intent(action_usb_permission), 0);
-            usbManager.requestPermission(usbAccessory, broadcast);
+            usbManager.requestPermission(usbDevice, broadcast);
         } else {
             Toast.makeText(UsbtActivity.this, "already have permission", Toast.LENGTH_SHORT).show();
         }
     }
 
-    private FileInputStream mFileInputStream;
 
-    private FileOutputStream mFileOutputStream;
-
-    /**
-     * 连接设备
-     * @param usbAccessory
-     * @return
-     */
-    public boolean connect(UsbAccessory usbAccessory) {
-        ParcelFileDescriptor parcelFileDescriptor = usbManager.openAccessory(usbAccessory);
-        if (parcelFileDescriptor != null) {
-            FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
-
-            mFileInputStream = new FileInputStream(fileDescriptor);
-
-            mFileOutputStream = new FileOutputStream(fileDescriptor);
-
+    public boolean connect(UsbDevice mUsbDevice) {
+        if (mUsbDevice != null) {
+            UsbInterface anInterface = mUsbDevice.getInterface(0);
+            mUsbDeviceConnection = usbManager.openDevice(mUsbDevice);//连接usb设备
+            if (mUsbDeviceConnection == null) {
+                Toast.makeText(UsbtActivity.this, "mUsbDeviceConnection can't be null", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+            if (mUsbDeviceConnection.claimInterface(anInterface, true)) {
+                Toast.makeText(UsbtActivity.this, "找到USB接口", Toast.LENGTH_SHORT).show();
+                int endpointCount = anInterface.getEndpointCount();
+                for (int i = 0; i < endpointCount; i++) {
+                    UsbEndpoint endpoint = anInterface.getEndpoint(i);
+                    if (endpoint.getType() == UsbConstants.USB_ENDPOINT_XFER_BULK) {
+                        if (UsbConstants.USB_DIR_IN == endpoint.getDirection()) {
+                            mUsbEndpoint_in = endpoint;//获取读数据通道
+                        } else {
+                            mUsbEndpoint_out = endpoint;//获取写数据通道
+                        }
+                    }
+                }
+                return true;
+            } else {
+                mUsbDeviceConnection.close();//关闭连接
+                Toast.makeText(UsbtActivity.this, "找不到USB接口", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(UsbtActivity.this, "mUsbDevice can't be null", Toast.LENGTH_SHORT).show();
         }
         return false;
     }
 
     /**
-     * 写入数据
-     * @param data
+     * 从usb通信设备中读取数据
+     * @return
      */
-    public void write(byte[] data) {
-        try {
-            mFileOutputStream.write(data);
-        } catch (IOException e) {
-            e.printStackTrace();
+    public byte[] readData() {
+        int inMax = mUsbEndpoint_in.getMaxPacketSize();
+        byte[] bytes = new byte[inMax];
+        ByteBuffer byteBuffer = ByteBuffer.allocate(inMax);
+        UsbRequest usbRequest = new UsbRequest();
+        usbRequest.initialize(mUsbDeviceConnection, mUsbEndpoint_in);
+        usbRequest.queue(byteBuffer, inMax);
+        if (mUsbDeviceConnection.requestWait() == usbRequest) {
+            bytes = byteBuffer.array();
         }
-
+        return bytes;
     }
 
     /**
-     * 读取数据
+     * 将数据写入到usb设备中
+     * @param bytes
      */
-    public void read() {
-        int max=1000;//字节数据根据协议来确定
-        byte[] data = new byte[max];
-        try {
-            mFileInputStream.read(data);
-        } catch (IOException e) {
-            e.printStackTrace();
+    public void sendData(byte[] bytes) {
+        if (mUsbDeviceConnection == null) {
+            Toast.makeText(UsbtActivity.this, "mUsbDeviceConnection can't be null", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (mUsbEndpoint_out == null) {
+            Toast.makeText(UsbtActivity.this, "mUsbEndpoint_out can't be null", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        int i = mUsbDeviceConnection.bulkTransfer(mUsbEndpoint_out, bytes, bytes.length, 1000);
+        if (i < 0) {
+            Toast.makeText(UsbtActivity.this, "failure to write", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(UsbtActivity.this, "success to write", Toast.LENGTH_SHORT).show();
         }
     }
-
 
     /**
      * 权限广播接收器
      */
-    private BroadcastReceiver mMyBroadcastReceiver = new BroadcastReceiver() {
+    private BroadcastReceiver mMyBroadcastReceiver=new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
             if (action_usb_permission.equals(action)) {
                 synchronized (this) {
-                    UsbAccessory usbAccessory = intent.getParcelableExtra(UsbManager.EXTRA_ACCESSORY);
-                    if (usbAccessory != null) {
+                    UsbDevice usbDevice = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+                    if (usbDevice != null) {
                         if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
-                            connect(usbAccessory);
+                            connect(usbDevice);
                             Toast.makeText(context, "success get permission", Toast.LENGTH_SHORT).show();
                         } else {
                             Toast.makeText(context, "failure get permission", Toast.LENGTH_SHORT).show();
@@ -198,14 +230,20 @@ public class UsbtActivity extends AppCompatActivity {
 
     /**
      * 筛选出我们想要的usb设备
-     *
-     * @param usbAccessory
+     * @param name
+     * @param usbDevice
      * @return
      */
-    public boolean filterDevice(UsbAccessory usbAccessory) {
+    public boolean filterDevice(String name, UsbDevice usbDevice) {
         // TODO: 2019/3/13 对应判断设备是否是我们要连接的设备
+       if ((usbDevice.getVendorId()==1423) && (usbDevice.getProductId()==25479))
+           return  true;
         return false;
     }
+
+
+
+
 
 
 }
